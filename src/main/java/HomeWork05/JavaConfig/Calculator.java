176f9d6c8faddb37d7a0a2d88efc5aa08681faea
ANDREY <andrey.kiyas@gmail.com>
2022-08-15 12:42:08 +0300

    PB03 "Home Work 05 - JavaConfig"

--- src/main/java/HomeWork05/JavaConfig/Calculator.java
+++ src/main/java/HomeWork05/JavaConfig/Calculator.java
@@ -16,16 +16,8 @@ public class Calculator {
         return a;
     }
 
-    public void setA(int a) {
-        this.a = a;
-    }
-
     public int getB() {
         return b;
-    }
-
-    public void setB(int b) {
-        this.b = b;
     }
 
     public void getCalc() {
